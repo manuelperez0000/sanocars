cd4686deb6a8116a7d83f4c81ffd3c0394799be7
manuel perez <manuelperez.0000@gmail.com>
2025-12-08 10:18:05 -0400

    encabezado faltante

--- src/assets/facturaTemplate.js
+++ src/assets/facturaTemplate.js
@@ -7,6 +7,7 @@ export const headerFactura = ({id,titulo}) => {
                             <div>${new Date().toLocaleDateString('es-VE')} ${new Date().toLocaleTimeString('es-VE')}</div>
                         </div>
                         <div class="japones" style="text-align: right;">
+                            （登録番号 T4080101020530) <br/>
                             <h3>株式会社SANOCARS沼津本店</h3>
                             静岡県沼津市井出1165番地3 <br>
                             TEL055-957-2206 / FAX055-957-2207 <br>
@@ -15,7 +16,7 @@ export const headerFactura = ({id,titulo}) => {
                             お支払金融機関名 / お支払期限日】<br>
                             静岡銀行  広見支店<br>
                             (普通預金) 0596664 カ）サノカーズ<br>
-                            お支払期限日： 一週間<br>
+                            お支払期限日：  一週間<br>
                         </div>
                     </div>
                 </div>`
