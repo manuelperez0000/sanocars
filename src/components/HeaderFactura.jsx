0c424a6d190336ad4d8d40bb6c4427580ebc4da9
manuel perez <manuelperez.0000@gmail.com>
2025-12-08 10:31:26 -0400

    facturas

--- src/components/HeaderFactura.jsx
+++ src/components/HeaderFactura.jsx
@@ -7,6 +7,7 @@ const HeaderFactura = ({ id }) => {
                     <div>{new Date().toLocaleDateString('es-VE')} {new Date().toLocaleTimeString('es-VE')}</div>
                 </div>
                 <div className="japones" style={{ textAlign: "right" }}>
+                    (登録番号 T4080101020530) <br /> 
                     <h3>株式会社SANOCARS沼津本店</h3>
                     静岡県沼津市井出1165番地3 <br />
                     TEL055-957-2206 / FAX055-957-2207 <br />
